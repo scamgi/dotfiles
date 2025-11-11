e06261c87382083d22065a120799aa0c129dbeb7
Giacomo Scampini <scampini.giacomo@gmail.com>
2025-11-11 15:24:44 +0100

    feat: add aliases of zsh

---
+++ zsh/.aliases.zsh
@@ -0,0 +1,44 @@
+# General Aliases
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+alias ls='ls -G'
+alias l='ls -lah'
+alias ll='ls -l'
+alias la='ls -A'
+
+# Grep
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Git Aliases
+alias g='git'
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gbd='git branch -d'
+alias gc='git commit'
+alias gcmsg='git commit -m'
+alias gco='git checkout'
+alias gcp='git cherry-pick'
+alias gd='git diff'
+alias gda='git diff HEAD'
+alias gf='git fetch'
+alias gg='git grep'
+alias gl='git pull'
+alias gp='git push'
+alias gpo='git push origin'
+alias gr='git remote'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gst='git status'
+
+# System
+alias sudo='sudo '
+alias df='df -h'
+alias du='du -h'
+
+# Process Management
+alias top='htop'
