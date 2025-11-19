2cb57ef74e744de524d699ca8dab2f97e3533773
Giacomo Scampini <scampini.giacomo@gmail.com>
2025-11-19 15:23:22 +0100

    fix: FINALLY CATPPUCCIN ON NEOVIM WORKS!

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,13 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    flavour = "mocha", -- Set the flavour to "latte" here
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
+}
