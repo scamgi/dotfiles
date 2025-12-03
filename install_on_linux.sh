dbbdf84d6c5b6432954a2bb06aa7a7f400257400
Giacomo <scampini.giacomo@gmail.com>
2025-12-03 16:02:36 +0100

    feat: put the list of all the packages inside the linux shell script

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
-echo "Hello world!"
+# install packages of Brewfile
+
+yay -Sy go \
+  nodejs \
+  starship \
+  vim \
+  tmux \
+  pandoc \
+  ncdu \
+  github-cli \
+  lazygit \
+  kubectl \
+  helm \
+  tokei \
+  fish \
+  cbonsai \
+  zoxide \
+  fzf \
+  parallel \
+  yazi \
+  neovim \
+  tldr \
+  eza \
+  bat \
+  fd \
+  ripgrep \
+  rust \
+  fastfetch \
+  htop
+# watch \ # not available in yay
+lazydocker \
+  btop \
+  k9s
+
+sudo pacman -S git-delta
+sudo pacman -S ttf-jetbrains-mono # to install jetbrains mono
+
+# install bun
+curl -fsSL https://bun.sh/install | bash
--- scripts/fish/set_theme_fish.sh
+++ scripts/fish/set_theme_fish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env fish
 
 # Set the theme to Mocha (or replace with Latte, Frappe, or Macchiato)
 fish_config theme save "Catppuccin Mocha"
